ba66d580f3c8f92951c0cbb3bf2fe5ab8e611e88
Paul Hawk <hawkpaul@gmail.com>
2025-03-30 21:30:58 -0500

    work

--- src/config.ts
+++ src/config.ts
@@ -1,75 +1,74 @@
-const flap_spool_pitch_radius = (
-  number_of_flaps: number,
-  hole_radius: number,
-  hole_separation: number
+const flapSpoolPitchRadius = (
+  numberOfFlaps: number,
+  holeRadius: number,
+  holeSeparation: number
 ): number =>
-  (number_of_flaps * (hole_radius * 2 + hole_separation)) / (2 * Math.PI);
+  (numberOfFlaps * (holeRadius * 2 + holeSeparation)) / (2 * Math.PI);
 
-const flap_spool_outer_radius = (
-  number_of_flaps: number,
-  hole_radius: number,
-  hole_separation: number,
+const flapSpoolOuterRadius = (
+  numberOfFlaps: number,
+  holeRadius: number,
+  holeSeparation: number,
   outset: number
 ): number =>
-  flap_spool_pitch_radius(number_of_flaps, hole_radius, hole_separation) +
-  hole_radius +
+  flapSpoolPitchRadius(numberOfFlaps, holeRadius, holeSeparation) +
+  holeRadius +
   outset;
 
-const flap_defaults = {
-  corner_radius: 3.1,
+const flapDefaults = {
+  cornerRadius: 3.1,
   height: 43,
-  height_auto: false,
-  notch_height_auto: false,
-  notch_height_default: 15,
-  notch_depth: 3.2,
-  number_of_flaps: 52,
-  pin_width: 1.4,
+  heightAuto: false,
+  notchHeightAuto: false,
+  notchHeightDefault: 15,
+  notchDepth: 3.2,
+  numberOfFlaps: 52,
+  pinWidth: 1.4,
   thickness: (30 / 1000) * 25.4,
   width: 54,
 };
 
-const font_defaults = {};
-const spool_defaults = {
-  hole_separation: 1.2,
+const fontDefaults = {};
+const spoolDefaults = {
+  holeSeparation: 1.2,
   outset: 0.8,
 };
 
-export const get_config = (
-  font_overrides = {},
-  flap_overrides = {},
-  spool_overrides = {}
+export const getConfig = (
+  fontOverrides = {},
+  flapOverrides = {},
+  spoolOverrides = {}
 ) => {
-  const flap_config = {
-    ...flap_defaults,
-    ...flap_overrides,
+  const flapConfig = {
+    ...flapDefaults,
+    ...flapOverrides,
   };
-  const spool_config = {
-    ...spool_defaults,
-    ...spool_overrides,
+  const spoolConfig = {
+    ...spoolDefaults,
+    ...spoolOverrides,
   };
-  const hole_radius = (flap_config.pin_width + 0.8) / 2;
+  const holeRadius = (flapConfig.pinWidth + 0.8) / 2;
 
   return {
     characters: [],
-    flap_config: {
-      ...flap_config,
-      gap:
-        hole_radius * 2 - flap_config.pin_width + spool_config.hole_separation,
+    flapConfig: {
+      ...flapConfig,
+      gap: holeRadius * 2 - flapConfig.pinWidth + spoolConfig.holeSeparation,
     },
-    font_config: { ...font_defaults, ...font_overrides },
-    spool_config: {
-      ...spool_config,
-      hole_radius,
-      outer_radius: flap_spool_outer_radius(
-        flap_config.number_of_flaps,
-        hole_radius,
-        spool_config.hole_separation,
-        spool_config.outset
+    fontConfig: { ...fontDefaults, ...fontOverrides },
+    spoolConfig: {
+      ...spoolConfig,
+      holeRadius,
+      outerRadius: flapSpoolOuterRadius(
+        flapConfig.numberOfFlaps,
+        holeRadius,
+        spoolConfig.holeSeparation,
+        spoolConfig.outset
       ),
-      pitch_radius: flap_spool_pitch_radius(
-        flap_config.number_of_flaps,
-        hole_radius,
-        spool_config.hole_separation
+      pitchRadius: flapSpoolPitchRadius(
+        flapConfig.numberOfFlaps,
+        holeRadius,
+        spoolConfig.holeSeparation
       ),
     },
   };
