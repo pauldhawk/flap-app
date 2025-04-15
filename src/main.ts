29747db5b6e780633c0ab0e3ac91db3d622f7cbc
Paul Hawk <hawkpaul@gmail.com>
2025-04-15 17:54:25 -0500

    work

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,10 @@
-// import * as THREE from "three";
-// import { getConfig } from "./config.ts";
-// import { drawFlap } from "./shapes/flap.ts";
-
-// // Get the global configs
-// const { flapConfig, printConfig } = getConfig();
-
-// const scene = new THREE.Scene();
-
-// // Set up the camera
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   window.innerWidth / window.innerHeight,
-//   0.1,
-//   1000
-// );
-// camera.position.z = 200;
-
-// // Set up the renderer
-// const renderer = new THREE.WebGLRenderer({ antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-
-// // Draw the flap shape:
-// const shape = drawFlap(flapConfig, printConfig);
-// scene.add(shape);
-// // Render loop
-// function animate() {
-//   requestAnimationFrame(animate);
-//   renderer.render(scene, camera);
-// }
-
-// animate();
+import { booleans, colors, primitives, transforms } from "@jscad/modeling";
 import {
-  prepareRender,
-  drawCommands,
   cameras,
+  drawCommands,
   entitiesFromSolids,
+  prepareRender,
 } from "@jscad/regl-renderer";
-import { colors, primitives, booleans, transforms } from "@jscad/modeling";
 const { cube, cuboid, line, sphere, star } = primitives;
 const { intersect, subtract } = booleans;
 const { colorize } = colors;
--- src/shapes/flap.ts
+++ src/shapes/flap.ts
@@ -1,66 +1,16 @@
-import * as THREE from "three";
 import { FlapConfig, PrintConfig } from "../schema";
 
 export function drawFlap(flapConfig: FlapConfig, printConfig: PrintConfig) {
   const { epsilon } = printConfig;
   const { cornerRadius, height, notchDepth, notchHeight, width } = flapConfig;
 
-  // Main flap shape with rounded top corners
-  const shape = new THREE.Shape();
-  shape.moveTo(0, 0);
-  shape.lineTo(width, 0);
-  shape.lineTo(width, height - cornerRadius);
-  shape.absarc(
-    width - cornerRadius,
-    height - cornerRadius,
+  console.log(
+    "drawFlap",
     cornerRadius,
-    0,
-    Math.PI / 2,
-    false
+    height,
+    notchDepth,
+    notchHeight,
+    width,
+    epsilon
   );
-  shape.lineTo(cornerRadius, height);
-  shape.absarc(
-    cornerRadius,
-    height - cornerRadius,
-    cornerRadius,
-    Math.PI / 2,
-    Math.PI,
-    false
-  );
-  shape.lineTo(0, 0);
-
-  // Notch shape Cutout /////////////////
-  // Left Notch
-
-  const notchShapeLeft = new THREE.Shape();
-  notchShapeLeft.moveTo(0, 0);
-  notchShapeLeft.lineTo(epsilon + notchDepth, 0);
-  notchShapeLeft.lineTo(epsilon + notchDepth, notchHeight);
-  notchShapeLeft.lineTo(0, notchHeight);
-  notchShapeLeft.lineTo(0, 0);
-
-  // Notch shape Cutout: Right Notch
-  const notchShapeRight = new THREE.Path();
-  const notchWidth = epsilon + notchDepth;
-  const xRight = width - notchWidth;
-
-  notchShapeRight.moveTo(xRight, 0);
-  notchShapeRight.lineTo(width, 0);
-  notchShapeRight.lineTo(width, notchHeight);
-  notchShapeRight.lineTo(xRight, notchHeight);
-  notchShapeRight.lineTo(xRight, 0);
-
-  // Add notches to shape as holes
-  shape.holes.push(notchShapeLeft);
-  shape.holes.push(notchShapeRight);
-
-  // Create geometry and mesh
-  const shapeGeometry = new THREE.ShapeGeometry(shape);
-  const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    side: THREE.DoubleSide,
-  });
-  const mesh = new THREE.Mesh(shapeGeometry, material);
-
-  return mesh;
 }
